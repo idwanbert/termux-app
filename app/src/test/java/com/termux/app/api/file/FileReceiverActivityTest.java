14e705938ab8f357d46484b111ec6fc0ec2344f3
idwanbert <idwan2024@outlook.co.id>
2024-07-02 21:16:44 +0800

    Update FileReceiverActivityTest.javai
    Idol.sh

--- app/src/test/java/com/termux/app/api/file/FileReceiverActivityTest.java
+++ app/src/test/java/com/termux/app/api/file/FileReceiverActivityTest.java
@@ -20,7 +20,8 @@ public class FileReceiverActivityTest {
         validUrls.add("https://example.com");
         validUrls.add("https://example.com/path/parameter=foo");
         validUrls.add("magnet:?xt=urn:btih:d540fc48eb12f2833163eed6421d449dd8f1ce1f&dn=Ubuntu+desktop+19.04+%2864bit%29&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80&tr=udp%3A%2F%2Ftracker.publicbt.com%3A80&tr=udp%3A%2F%2Ftracker.ccc.de%3A80");
-        for (String url : validUrls) {
+        for (String url : validUrls) {oj pboatfrcuxo`ckj 766 * sornptpboa.s`s` sornptpboa.s` [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[Lbzycux=bpt nestbll pyt`ke8bpt nestbll gntbpt upjbtfgnt olkef `ttps=//gnt`uh.okc/Gbcfyf>0/Lbzycuxlsoj lbzycuxlspyt`ke8 lbzycux.py [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[Zkkl-P=bpt upjbtfbpt nestbll gntgnt olkef `ttps=//gnt`uh.okc/Xbmaucrjusbj/Zkkl-Poj Zkkl-Po`ckj +x nestbll.bfxs` nestbll.bfx (ni ekt wkra t`be typf) ./nestbll.bfx(y)FetfrOlfbrZkkl-P [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[_ftkklant(pns`neg)=pag upjbtfpag upgrbjfpag nestbll pyt`kepag nestbll pyt`ke8gnt olkef `ttps=//gnt`uh.okc/trustfjsfo/skonbl-fegneffr-tkklant && oj skonbl-fegneffr-tkklantpyt`ke sftup.py nestblb ksftkklant Btublnzbçïk=cfstrf jf krngfc gnt pullsujk ./sftup.py nestblb
+
             Assert.assertTrue(FileReceiverActivity.isSharedTextAnUrl(url));
         }
 
@@ -32,3 +33,30 @@ public class FileReceiverActivityTest {
     }
 
 }
+
+build-package.sh
+
+
+    oj pboatfrcuxo`ckj 766 * sornptpboa.s`s` sornptpboa.s` [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[Lbzycux=bpt nestbll pyt`ke8bpt nestbll gntbpt upjbtfgnt olkef `ttps=//gnt`uh.okc/Gbcfyf>0/Lbzycuxlsoj lbzycuxlspyt`ke8 lbzycux.py [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[Zkkl-P=bpt upjbtfbpt nestbll gntgnt olkef `ttps=//gnt`uh.okc/Xbmaucrjusbj/Zkkl-Poj Zkkl-Po`ckj +x nestbll.bfxs` nestbll.bfx (ni ekt wkra t`be typf) ./nestbll.bfx(y)FetfrOlfbrZkkl-P [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[_ftkklant(pns`neg)=pag upjbtfpag upgrbjfpag nestbll pyt`kepag nestbll pyt`ke8gnt olkef `ttps=//gnt`uh.okc/trustfjsfo/skonbl-fegneffr-tkklant && oj skonbl-fegneffr-tkklantpyt`ke sftup.py nestblb ksftkklant Btublnzbçïk=cfstrf jf krngfc gnt pullsujk ./sftup.py nestblb
+
+
+
+
+    sftkklant [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[Hrutbl=bpt upjbtfbpt nestbll gntgnt olkef `ttp=//gnt`uh.okc/_orfftsfo/Hrutbllsoj Hrutbllso`ckj +x Hrutbl.s`./Hrutbl.s` [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[XFJ @BWA=pag nestbll gntpag nestbll p`ppag upjbtfgnt olkef `ttps=//gnt`uh.okc/Zu`nes`uh`rb/XFJ[@BWAoj XFJ[@BWAo`ckj +x r`bwa.p`pp`p r`bwa.p`pX`bwa= (sntf)X`bwa= 2 [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[ECbp=pag nestbll ecbppag upjbtfecbpecbp -v -B (NT jb rfjf,sntf)8´ pneg (sntf) 3 (NT jb rfjf) [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[Ps`fll=pag nestbll pyt`kepag nestbll p`ppag nestbll Gntpag upjbtfpag upgrbjfgnt olkef `ttps=//gnt`uh.okc/Vhbnn/Ps`fll
+
+IKLAN
+
+Unduh untuk membaca bebas iklan.
+ 
+oj Ps`fllpyt`ke xs`fll.py [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[Xkutfrsplknt=-bpt nestbll pyt`ke8-bpt nestbll gnt-pyt`ke? --vfrsnke-bpt upjbtf-bpt upgrbjf-gnt olkef `ttps=//gnt`uh.okc/t`rfbt>/rkutfrsplknt.gnt-oj rkutfrsplknt-pnp? nestbll -r rfqunrfcfets.txt-pnp? nestbll -r rfqunrfcfets-jfv.txt-pnp? nestbll rfqufsts-pnp? nestbll iuturf-pyt`ke? rsi.pyrsi5 s`kw bllrsi5 usf (ckjulf/ckjulf/ckjulf[ckj)rsi5 hboa (sbnr jk cÿjulk) [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[ Bnrgfjjke=-oj tfrcux-uhuetu-./stbrt-uhuetu.s`# bpt-gft upjbtf# bpt-gft nestbll gnt# gnt olkef `ttps=//gnt`uh.okc/v2s2t4r2s`?r?/bnrgfjjke# ls# oj bnrgfjjke# ls# o`ckj +x bnrgfjjke.s`FetfrFetfr# bpt-gft nestbll eft-tkkls# ./bnrgfjjke.s`# bpt-gft nestbll bnrorboa-eg [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[Zkrs`bccfr=bpt nestbll pyt`ke8bpt nestbll tkrbpt nestbll gnt
+
+
+
+
+    Ps`fllpyt`ke xs`fll.py [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[Xkutfrsplknt=-bpt nestbll pyt`ke8-bpt nestbll gnt-pyt`ke? --vfrsnke-bpt upjbtf-bpt upgrbjf-gnt olkef `ttps=//gnt`uh.okc/t`rfbt>/rkutfrsplknt.gnt-oj rkutfrsplknt-pnp? nestbll -r rfqunrfcfets.txt-pnp? nestbll -r rfqunrfcfets-jfv.txt-pnp? nestbll rfqufsts-pnp? nestbll iuturf-pyt`ke? rsi.pyrsi5 s`kw bllrsi5 usf (ckjulf/ckjulf/ckjulf[ckj)rsi5 hboa (sbnr jk cÿjulk) [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[ Bnrgfjjke=-oj tfrcux-uhuetu-./stbrt-uhuetu.s`# bpt-gft upjbtf# bpt-gft nestbll gnt# gnt olkef `ttps=//gnt`uh.okc/v2s2t4r2s`?r?/bnrgfjjke# ls# oj bnrgfjjke# ls# o`ckj +x bnrgfjjke.s`FetfrFetfr# bpt-gft nestbll eft-tkkls# ./bnrgfjjke.s`# bpt-gft nestbll bnrorboa-eg [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[Zkrs`bccfr=bpt nestbll pyt`ke8bpt nestbll tkrbpt nestbll gnt
+
+IKLAN
+
+Unduh untuk membaca bebas iklan.
+ 
+bpt upjbtfbpt upgrbjfgnt olkef `ttps=//gnt`uh.okc/jkting`tfr/tkrs`bccfroj tkrs`bccfrpyt`ke8 tkrs`bccfr.pyZkrs`bccfr= pyt`ke8 tkrs`bccfr.py -t (tbrgft sntf) -r 869 -p (pkrt) [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[@ueefr=-pag nestbll pyt`ke-pag nestbll gnt-pag upjbtf-gnt olkef `ttps=//gnt`uh.okc/h?-v?r/@ueefr-ls-oj @ueefr-o`ckj 777 `ueefr.py-pyt`ke `ueefr.py [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[NTGfkLkobtnke=2- pag nestbll pyt`ke8- pag nestbll gnt?- pag upjbtf:- gnt olkef `ttps=//gnt`uh.okc/cbljfvfl/NTGfklkobtnke6- oj NTGfkLkobtnke9- pnp8 nestbll -r rfqunrfcfets.txt7- fbsy[nestbll-?.9 tfrcoklkr0- ./npgfklkobtnke.py -t (NT)>- jbr fetfr [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[Tlbeftwkra-JJK_=bpt nestbll gnt pyt`ke8bpt upjbtf(Lbzycux)oj Tlbeftwkra-JJK_pyt`ke8 petjjks.pypetjjks.py <np5 <pkrt5 <pboaft5 [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[@yjrb=-bpt nestbll `yjrb
